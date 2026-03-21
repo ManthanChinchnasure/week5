886523f2ba64018b7389dee3d5834d1a9d7e3f32
ManthanChinchansure <manthanchinchansure68@gmail.com>
2026-03-21 12:29:35 +0530

    write promise

--- promiseWritefile.js
+++ promiseWritefile.js
@@ -1,5 +1,23 @@
 // fs readfilePromisified..
-// fs write
+// fs writefielpromise..
 
 
-function fsReadAndWrite
+function fsReadAndWritePromise(filepath,encoding) {
+    return  new Promise (function (resolve,reject){
+        fsReadFilePromisified(filepath, encoding)
+        .then(function (content){
+            content = content.toUpperclass();
+            fsWriteFilePromisified(filepath,content)
+            resolve()
+        })
+        
+
+    })
+}
+
+fsReadAndWriteFilePromisified("a.txt", "utf-8")
+
+
+
+// ReferenceError: fsReadAndWriteFilePromisified is not defined
+// becacuse we not define the function so yet it not call to excute the programe...
