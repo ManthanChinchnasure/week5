37f9085b1107341139922976fffadd5661e0f77e
ManthanChinchansure <manthanchinchansure68@gmail.com>
2026-03-22 16:47:56 +0530

    async approach of own promise

---
+++ ownpromise/asyncown.js
@@ -0,0 +1,18 @@
+
+// it async approach..
+
+
+const fs = require("fs");
+
+fs.readFile("a.txt","utf-8", function (err, Contents){
+    const trimmedContents = Contents.trim();
+    fs.writeFile("a.txt", trimmedContents,function(){
+        console.log("done!!!");
+    });
+
+    });
+
+
+
+    // in this code we have to done the basic part which we remove the extra space here 
+    // we disscus the anotherr approach of it..
