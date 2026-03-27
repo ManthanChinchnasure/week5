27a4bffaa1e4c627a5e907594eea6402f2a81c6c
ManthanChinchansure <manthanchinchansure68@gmail.com>
2026-03-27 10:48:09 +0530

    assignment

--- try.js
+++ try.js
@@ -16,28 +16,28 @@ fs.readFile("a.txt", "utf-8", function (err,data){
 
 */
 
-function fscall(filepath,encoding){
-    return new Promise (function (resolve,reject){
-        fs.readFile(filepath,encoding,function (err,data){
-            if (err){
-                reject(err)
-            } else {
-                resolve (data)
-            }
-        })
+// function fscall(filepath,encoding){
+//     return new Promise (function (resolve,reject){
+//         fs.readFile(filepath,encoding,function (err,data){
+//             if (err){
+//                 reject(err)
+//             } else {
+//                 resolve (data)
+//             }
+//         })
 
-    });
-}
+//     });
+// }
 
 
-fscall("a.txt", "utf-8")
-  .then(function (data){
-  console.log(data);
-  })
+// fscall("a.txt", "utf-8")
+//   .then(function (data){
+//   console.log(data);
+//   })
 
-  .catch(function (err){
-    console.log("get the error")
-  });
+//   .catch(function (err){
+//     console.log("get the error")
+//   });
 
 
  
